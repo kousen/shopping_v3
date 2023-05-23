823ad6eca4489a8c932140b37b4f09a963939c87
Ken Kousen <ken.kousen@kousenit.com>
2023-05-23 14:42:06 -0400

    Update test for testcontainers

--- src/test/java/com/oreilly/shopping/dao/ProductRepositoryTest.java
+++ src/test/java/com/oreilly/shopping/dao/ProductRepositoryTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(ContainersConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProductRepositoryTest {
